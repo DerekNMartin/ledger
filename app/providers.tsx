d05195c9356fbe33912d65d39df2cd5d1f2e94b7
Derek Martin <derek.n.martin@gmail.com>
2025-12-28 12:10:55 -0500

    fix: try fixing styles?

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,12 @@
 'use client';
-
+import { useState } from 'react';
 import { HeroUIProvider } from '@heroui/react';
 import { AuthProvider } from '@/auth/context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+  // This ensures the queryClient is only created once per session
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <HeroUIProvider>
